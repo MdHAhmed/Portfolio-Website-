81cd740d9bacad3ecc09b54c6f15fcca0dcc5170
Md Hassan Ahmed (Rafi) <mdhassanahmed2929@gmail.com>
2018-01-17 19:27:40 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,135 @@
+
+function main() {
+
+(function () {
+   'use strict';
+
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top-40
+          }, 900);
+            return false;
+          }
+        }
+      });
+
+	// affix the navbar after scroll below header
+$('#nav').affix({
+      offset: {
+        top: $('header').height()
+      }
+});
+
+	// skills chart
+	$(document).ready(function(e) {
+	//var windowBottom = $(window).height();
+	var index=0;
+	$(document).scroll(function(){
+		var top = $('#skills').height()-$(window).scrollTop();
+		// console.log(top)
+		if(top<-300){
+			if(index==0){
+
+				$('.chart').easyPieChart({
+					easing: 'easeOutBounce',
+					onStep: function(from, to, percent) {
+						$(this.el).find('.percent').text(Math.round(percent));
+					}
+				});
+
+				}
+			index++;
+		}
+	})
+	//console.log(nagativeValue)
+	});
+
+
+  	// Portfolio isotope filter
+    $(window).load(function() {
+        var $container = $('.portfolio-items');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.cat a').click(function() {
+            $('.cat .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+
+    });
+
+
+    // CounterUp
+	$(document).ready(function( $ ) {
+		if($("span.count").length > 0){
+			$('span.count').counterUp({
+					delay: 10, // the delay time in ms
+			time: 1500 // the speed time in ms
+			});
+		}
+	});
+
+  	// Pretty Photo
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false
+	});
+
+    document.getElementById('more').onclick = function(){
+        document.getElementById("extra1").style.display = "block";
+        document.getElementById("extra2").style.display = "block";
+        document.getElementById("extra3").style.display = "block";
+        document.getElementById("extra4").style.display = "block";
+        document.getElementById("extra5").style.display = "block";
+        document.getElementById("more").style.display = "none";
+    }
+
+    document.getElementById('sendEmailButt').onclick = function(){
+        console.log("send mail");
+    //     $.ajax({
+    //       type: “POST”,
+    //       url: "https://mandrillapp.com/api/1.0/messages/send.json",
+    //       data: {
+    //         'key': 'cc76cbd7c2749bd884b03d264bc2f35f-us17',
+    //         'message': {
+    //           'from_email': 'shadmanmahdi264@gmail.com',
+    //           'to': [
+    //               {
+    //                 'email': 'md.ahmed1@yorkmail.cuny.edu',
+    //                 'name': 'asshole',
+    //                 'type': 'hoga'
+    //               }
+    //             ],
+    //           'autotext': 'true',
+    //           'subject': 'amar bal!',
+    //           'html': 'bal banao quick!'
+    //         }
+    //       }
+    //      }).done(function(response) {
+    //        console.log(response); // if you're into that sorta thing
+    //    });
+    }
+
+}());
+
+
+}
+main();
